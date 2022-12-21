64baaff6bbe50fb4ffb1259199ec5029476eb847
Dmitri Sokolov <mitson.mbox@gmail.com>
2022-12-21 19:46:27 +0300

    chore: Add .editorconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/i,
-                exclude: /node_modules/,
-                loader: 'ts-loader',
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/views/index.html',
-        }),
+  mode: 'development',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/i,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/views/index.html',
+    }),
+  ],
 };
