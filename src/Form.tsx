70f0f911ee0a60650249cf598c506a84f6fb779a
Dmitri Sokolov <mitson.mbox@gmail.com>
2022-12-23 20:34:54 +0300

    feat: Control input

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,36 +1,53 @@
+import {FormEvent} from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { ChangeEvent, useState } from 'react';
 
 function BasicExample() {
-    return (
-        <Container>
-            {/* <Row> */}
-                {/* <Col lg={6} xl={4}> */}
-                    <Form>
-                        <Form.Group className='mb3' controlId='formBasicEmail'>
-                            <Form.Label>Email Address</Form.Label>
-                            <Form.Control type='email' placeholder='Enter email' />
-                            <Form.Text className='text-muted'>
-                                We'll never share your email with anyone else.
-                            </Form.Text>
-                        </Form.Group>
-                        <Form.Group className='mb3' controlId='formBasicPassword'>
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type='password' placeholder='Password' />
-                        </Form.Group>
-                        <Form.Group className='mb3' controlId='formBasicChecbox'>
-                            <Form.Check type='checkbox' label='Check me out' />
-                        </Form.Group>
-                        <Button type='submit' variant='primary'>
-                            Submit
-                        </Button>
-                    </Form>
-                {/* </Col> */}
-            {/* </Row> */}
-        </Container>
-    );
+  const [email, setEmail] = useState('');
+  const handleInputChange = (event: ChangeEvent) => {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+    setEmail(input.value);
+  };
+  const handleSubmit = (o: FormEvent<HTMLFormElement>) => {
+    o.preventDefault();
+    console.log(o);
+    
+  }
+  return (
+    <Container className='pt-5'>
+      <Form
+        onSubmit={handleSubmit}
+      >
+        <Form.Group className="mb3" controlId="formBasicEmail">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={handleInputChange}
+          />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid email.
+          </Form.Control.Feedback>
+          {/* <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text> */}
+        </Form.Group>
+        <Form.Group className="mb3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb3" controlId="formBasicChecbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button type="submit" variant="primary">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
 }
 export default BasicExample;
