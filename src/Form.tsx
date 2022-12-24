c21719066304ae6589cedc43fe272c95d9bd3ca6
Dmitri Sokolov <mitson.mbox@gmail.com>
2022-12-24 14:26:49 +0300

    feat: Use react-hook-form

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,46 +1,74 @@
-import {FormEvent} from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
-import { ChangeEvent, useState } from 'react';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+type FormValues = {
+  email: string;
+  password: string;
+};
 
 function BasicExample() {
-  const [email, setEmail] = useState('');
-  const handleInputChange = (event: ChangeEvent) => {
-    const input = event.target as HTMLInputElement;
-    console.log(input.value);
-    setEmail(input.value);
-  };
-  const handleSubmit = (o: FormEvent<HTMLFormElement>) => {
-    o.preventDefault();
-    console.log(o);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FormValues>({
+    defaultValues: {
+      email: '',
+      password: '',
+    }
+  });
+  console.log(errors);
+
+  const formSubmit = (data: any) => {
+    reset();
+    console.log(data);
     
-  }
+  };
   return (
-    <Container className='pt-5'>
-      <Form
-        onSubmit={handleSubmit}
-      >
+    <Container className="pt-5">
+      <Form onSubmit={handleSubmit(formSubmit)}>
         <Form.Group className="mb3" controlId="formBasicEmail">
           <Form.Label>Email Address</Form.Label>
           <Form.Control
             type="email"
+            isInvalid={errors.email !== undefined}
             placeholder="Enter email"
-            value={email}
-            onChange={handleInputChange}
+            {...register('email', {
+              required: 'Пожалуйста, заполните поле',
+              pattern: {
+                value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                message: 'Неверный формат email'
+              }
+            })}
           />
           <Form.Control.Feedback type="invalid">
-            Please provide a valid email.
+            {String(errors.email?.message)}
           </Form.Control.Feedback>
-          {/* <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text> */}
         </Form.Group>
         <Form.Group className="mb3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control 
+            type="password"
+            isInvalid={errors.password !== undefined}
+            placeholder="Password"
+            {...register('password'), {
+              required: true,
+              validate: {
+                matchPattern: (value: string) => /^.+$/.test(value),
+              }
+            }}
+          />
+          <Form.Control.Feedback type="invalid">
+            {String(errors.password?.message)}
+          </Form.Control.Feedback>
         </Form.Group>
-        <Form.Group className="mb3" controlId="formBasicChecbox">
+        <Form.Group className="mb3" controlId="formBasicChecbox" onChange={() => {
+          console.log(errors)
+        }}>
           <Form.Check type="checkbox" label="Check me out" />
         </Form.Group>
         <Button type="submit" variant="primary">
